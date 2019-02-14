00c3d90356f1db96d32d41c652378b1414fc599e
Bill Mortimer <bill@billisit.com>
2019-02-13 18:34:12 -0800

    Seeding script is working from npm run db:setup script in package.json.

--- database/Movie.js
+++ database/Movie.js
@@ -7,12 +7,16 @@ mongoose.Promise = global.Promise;
 const movieSchema = new mongoose.Schema({
   id: { type: Number, index: { unique: true } },
   title: String,
-  year: String,
+  year: Number,
   imdb_id: String,
   mc_slug: String,
   trailers: Array,
+},
+{
+  timestamps: true
 });
 
 const Movie = mongoose.model('Movie', movieSchema);
 
 module.exports = Movie;
+
--- utils/seed.js
+++ utils/seed.js
@@ -1,1 +1,61 @@
-// Add code to seed the DB with movie and trailer data here.
+const Promise = require('bluebird');
+const path = require('path');
+const fs = Promise.promisifyAll(require('fs'));
+const XLSX = require('xlsx');
+const db  = require('../database/index.js');
+
+const Movie = require('../database/Movie.js');
+//const Trailer = require('../database/Trailer.js');
+
+const movieFilePath = path.join((`${__dirname}/../utils/movies.xlsx`)); 
+const trailerFilePath = path.join((`${__dirname}/../utils/trailers.xlsx`));
+
+// Read an Excel file and convert it to an array of objects
+const excelFileToArray = function (filePath) {
+  let workbook = XLSX.readFile(filePath);
+  let sheet_name_list = workbook.SheetNames;
+  let rows = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+  rows = JSON.stringify(rows);
+  let obj = JSON.parse(rows)
+  return obj;
+}
+
+// Read the movies spreadsheet and populate an array of movie objects
+var movieArray = excelFileToArray(movieFilePath);
+//console.log((movieArray));
+
+// trailer spreadsheet and populate an array of trailer objects
+var trailerArray = excelFileToArray(trailerFilePath);
+//console.log(trailerArray);
+
+// reduce the trailerArray to an object with the movie_id as the key and the array of trailers for that key as the value
+let trailerArrObj = trailerArray.reduce(function(acc, trailer) {
+  let key= trailer.movie_id;
+  if (!acc[key]) {
+    acc[key] = [];
+  } 
+  acc[key].push(trailer);
+  return acc;
+},{});
+
+
+
+movieArray.map(function(movie) {
+  // Map the populated trailer array onto each movieArray object, or an empty one if no trailers.
+  if (trailerArrObj[movie.id]) {
+    movie.trailers = trailerArrObj[movie.id];
+  } else {
+    movie.trailers = [];
+  }
+});
+
+// Insert all the movie into the database
+const insertMovies = function() {
+  Movie.create(movieArray)
+    .then(() => db.disconnect());
+};
+
+// Add drop collection here
+
+insertMovies();
+console.log('movies inserted into mongoDB');
