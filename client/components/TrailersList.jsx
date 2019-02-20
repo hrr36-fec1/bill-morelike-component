f0d13158f7f076fe1e3044c74c6d22288fecf32e
Bill Mortimer <bill@billisit.com>
2019-02-19 20:52:03 -0800

    Just cleaning up a couple of files.

--- client/components/TrailersList.jsx
+++ client/components/TrailersList.jsx
@@ -29,20 +29,3 @@ const TrailersList = (props) => {
 
 export default TrailersList;
 
-
-// <div class="image_strip_and_arrows trailers more_right">
-//   <div class="slider_arrow slider_previous"> 
-//     <i class="fa fa-angle-left"></i>
-//   </div>
-//   <div class="slider_arrow slider_next">
-//     <i class="fa fa-angle-right"></i>
-//   </div>
-//   <div class="image_strip_wrapper">
-//     <div class="slider_gradient slider_previous light"></div>
-//     <div class="slider_gradient slider_next light"></div>
-//     <div class="image_strip">
-
-//       <div class="clr"></div>
-//     </div>
-//   </div>
-// </div>
