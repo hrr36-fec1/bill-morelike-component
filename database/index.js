d6d626f7358110179b8d78e7a13c75579bc440f9
Bill Mortimer <bill@billisit.com>
2019-02-22 20:38:09 -0800

    Using DB on MongoDB Atlas instead of local DB.

--- database/index.js
+++ database/index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const MONGO_CONNECT_STRING = require('../.env/mongoConnect.js')
 
-const mongoUri = 'mongodb://localhost/betacritic';
+// const mongoUri = 'mongodb://localhost/betacritic';
+const mongoUri = MONGO_CONNECT_STRING;
 
 const db = mongoose.connect(mongoUri, { useNewUrlParser: true })
   .then(() => console.log('Connected to MongoDB'))
